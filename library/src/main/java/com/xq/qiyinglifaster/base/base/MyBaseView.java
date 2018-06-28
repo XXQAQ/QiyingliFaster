a740bb190ad2492e62d73cb6b3ee394c5b15f5e3
xq <120363542@qq.com>
2018-06-28 15:04:16 +0800

    edit BaseView

--- library/src/main/java/com/xq/qiyinglifaster/base/base/MyBaseView.java
+++ library/src/main/java/com/xq/qiyinglifaster/base/base/MyBaseView.java
@@ -7,8 +7,12 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.xq.customview.view.IconFontTextView;
 import com.xq.projectdefine.util.tools.BarUtils;
 import com.xq.projectdefine.util.tools.DensityUtils;
@@ -18,6 +22,8 @@ import com.xq.projectdefine.base.base.FasterBaseView;
 import com.yanzhenjie.statusview.StatusUtils;
 
 public abstract class MyBaseView<T extends IMyBasePresenter> extends FasterBaseView<T> implements IMyBaseView<T> {
+
+    protected Toolbar toolbar;
 
     protected NestedScrollView sv;
 
@@ -29,7 +35,10 @@ public abstract class MyBaseView<T extends IMyBasePresenter> extends FasterBaseV
     public void afterOnCreate(Bundle savedInstanceState) {
         super.afterOnCreate(savedInstanceState);
 
+        toolbar = (Toolbar) rootView.findViewById(getContext().getResources().getIdentifier("toolbar", "id", getContext().getPackageName()));
+
         sv = (NestedScrollView)findViewById(getContext().getResources().getIdentifier("sv", "id", getContext().getPackageName()));
+
         if (sv != null)
             sv.setNestedScrollingEnabled(false);
 
@@ -45,32 +54,64 @@ public abstract class MyBaseView<T extends IMyBasePresenter> extends FasterBaseV
     }
 
     @Override
-    public void initToolbar(String title, boolean isback) {
-        super.initToolbar(title, isback);
-        if (isback && getNavIcon() <= 0)
+    public void initToolbar(String title) {
+
+        initToolbar(title,true);
+    }
+
+    @Override
+    public void initToolbar(String title, boolean isShowIcon) {
+        if (toolbar == null)
+            return;
+
+        toolbar.setBackgroundColor(getToolbarBackgroundColor());
+
+        TextView tv_toolTitle = (TextView) toolbar.findViewById(getContext().getResources().getIdentifier("toolTitle", "id", getContext().getPackageName()));
+        if (tv_toolTitle == null)
         {
-            toolbar.setNavigationIcon(new BitmapDrawable(ImageUtils.view2Bitmap(new IconFontTextView.Bulder(getContext()).setText(getContext().getResources().getString(R.string.ic_return)).setTextColor(getToolbarWidgetColor()).setTextSize(DensityUtils.dip2px(getContext(),16)).build())));
+            toolbar.setTitle(title);
+            toolbar.setTitleTextColor(getToolbarWidgetColor());
         }
-    }
+        else
+        {
+            tv_toolTitle.setText(title);
+            tv_toolTitle.setTextColor(getToolbarWidgetColor());
+        }
 
-    @Override
-    public int getNavIcon() {
-        return 0;
-    }
+        if (isShowIcon)
+        {
+            if (getNavIcon() >0 )
+                toolbar.setNavigationIcon(getNavIcon());
+            else
+                toolbar.setNavigationIcon(new BitmapDrawable(ImageUtils.view2Bitmap(new IconFontTextView.Bulder(getContext()).setText(getContext().getResources().getString(R.string.ic_return)).setTextColor(getToolbarWidgetColor()).setTextSize(DensityUtils.dip2px(getContext(),16)).build())));
 
-    @Override
-    public int getToolbarBackgroundColor(){
-        return getContext().getResources().getColor(android.R.color.white);
-    }
-
-    @Override
-    public int getToolbarWidgetColor(){
-        return getContext().getResources().getColor(R.color.textcolor_normal);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ((AppCompatActivity)getContext()).finish();
+                }
+            });
+        }
     }
 
     @Override
     public String getBehaviorDescription() {
         return "";
+    }
+
+    //重写该方法以自定Toolbar图标
+    protected int getNavIcon() {
+        return 0;
+    }
+
+    //重写该方法以自定Toolbar背景色
+    protected int getToolbarBackgroundColor(){
+        return getContext().getResources().getColor(android.R.color.white);
+    }
+
+    //重写该方法以自定Toolbar控件颜色
+    protected int getToolbarWidgetColor(){
+        return getContext().getResources().getColor(R.color.textcolor_normal);
     }
 
     //重写该方法以沉浸状态栏
