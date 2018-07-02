5ef9fad140d6926d63fda07d7290f78842162418
xq <120363542@qq.com>
2018-07-02 14:35:12 +0800

    重量级更新

--- library/src/main/java/com/xq/qiyinglifaster/base/base/IMyBasePresenter.java
+++ library/src/main/java/com/xq/qiyinglifaster/base/base/IMyBasePresenter.java
@@ -1,10 +1,36 @@
 package com.xq.qiyinglifaster.base.base;
 
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import com.xq.projectdefine.base.base.IFasterBasePresenter;
 
 public interface IMyBasePresenter<T extends IMyBaseView> extends IFasterBasePresenter<T> {
 
+    @Override
+    default void afterOnCreate(Bundle savedInstanceState) {
+        IFasterBasePresenter.super.afterOnCreate(savedInstanceState);
+    }
 
+    @Override
+    default void onResume() {
+        IFasterBasePresenter.super.onResume();
+    }
+
+    @Override
+    default void onPause() {
+        IFasterBasePresenter.super.onPause();
+    }
+
+    @Override
+    default void onDestroy() {
+        IFasterBasePresenter.super.onDestroy();
+    }
+
+    @Override
+    default void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IFasterBasePresenter.super.onActivityResult(requestCode,resultCode,data);
+    }
 
 }
--- library/src/main/java/com/xq/qiyinglifaster/base/base/IMyBaseView.java
+++ library/src/main/java/com/xq/qiyinglifaster/base/base/IMyBaseView.java
@@ -1,9 +1,35 @@
 package com.xq.qiyinglifaster.base.base;
 
 
+import android.os.Bundle;
 import com.xq.projectdefine.base.base.IFasterBaseView;
 
 public interface IMyBaseView<T extends IMyBasePresenter> extends IFasterBaseView<T> {
+
+    @Override
+    default void afterOnCreate(Bundle savedInstanceState) {
+        IFasterBaseView.super.afterOnCreate(savedInstanceState);
+    }
+
+    @Override
+    default void onResume() {
+        IFasterBaseView.super.onResume();
+    }
+
+    @Override
+    default void onPause() {
+        IFasterBaseView.super.onPause();
+    }
+
+    @Override
+    default void onDestroy() {
+        IFasterBaseView.super.onDestroy();
+    }
+
+    @Override
+    default void onSaveInstanceState(Bundle outState) {
+        IFasterBaseView.super.onSaveInstanceState(outState);
+    }
 
     //手动调用以设置Toolbar标题
     public void initToolbar(String title);
