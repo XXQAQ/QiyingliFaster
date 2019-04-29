e664c498844bcc772a05bf9053f5c02b19b4e98a
XXQAQ <120363542@qq.com>
2019-04-29 16:30:15 +0800

    BarLayout与Toolbar的功能性API分开处理

--- library/src/main/java/com/xq/customfaster/base/base/CustomBaseView.java
+++ library/src/main/java/com/xq/customfaster/base/base/CustomBaseView.java
@@ -3,8 +3,11 @@ package com.xq.customfaster.base.base;
 import android.app.Activity;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import com.xq.androidfaster.base.base.FasterBaseView;
 import com.xq.androidfaster.util.tools.BarUtils;
@@ -12,6 +15,8 @@ import com.xq.androidfaster.util.tools.ImageUtils;
 import com.xq.androidfaster.util.tools.ScreenUtils;
 import com.xq.customview.view.IconFontTextView;
 import com.xq.customfaster.R;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class CustomBaseView<T extends ICustomBasePresenter> extends FasterBaseView<T> implements ICustomBaseView<T> {
 
@@ -33,6 +38,9 @@ public abstract class CustomBaseView<T extends ICustomBasePresenter> extends Fas
             //设置状态栏字体颜色
             setStatusBarLightMode(isLightStyle());
         }
+
+        initBarLayout();
+
     }
 
     @Override
@@ -45,27 +53,25 @@ public abstract class CustomBaseView<T extends ICustomBasePresenter> extends Fas
         if (toolbar == null)
             return;
 
-        if (getToolbarBackgroundResource() != 0)
-            toolbar.setBackgroundResource(getToolbarBackgroundResource());
-        else
-            toolbar.setBackgroundColor(getToolbarBackgroundColor());
-
-        TextView toolTitle = (TextView) toolbar.findViewById(getContext().getResources().getIdentifier("toolTitle", "id", getContext().getPackageName()));
-        if (toolTitle == null)
+        if (!TextUtils.isEmpty(title))
         {
-            toolbar.setTitle(title);
-            toolbar.setTitleTextColor(getToolbarWidgetColor());
-        }
-        else
-        {
-            toolTitle.setText(title);
-            toolTitle.setTextColor(getToolbarWidgetColor());
+            TextView toolTitle = (TextView) toolbar.findViewById(getContext().getResources().getIdentifier("toolTitle", "id", getContext().getPackageName()));
+            if (toolTitle == null)
+            {
+                toolbar.setTitle(title);
+                toolbar.setTitleTextColor(getToolbarWidgetColor());
+            }
+            else
+            {
+                toolTitle.setText(title);
+                toolTitle.setTextColor(getToolbarWidgetColor());
+            }
         }
 
         if (isShowIcon)
         {
-            if (getNavIcon() !=0 )
-                toolbar.setNavigationIcon(getNavIcon());
+            if (getToolbarNavIcon() !=0 )
+                toolbar.setNavigationIcon(getToolbarNavIcon());
             else
                 toolbar.setNavigationIcon(new BitmapDrawable(ImageUtils.view2Bitmap(new IconFontTextView.Bulder(getContext()).setText(getContext().getResources().getString(R.string.ic_return)).setTextColor(getToolbarWidgetColor()).setTextSize(ScreenUtils.dip2px(16)).build())));
 
@@ -83,19 +89,36 @@ public abstract class CustomBaseView<T extends ICustomBasePresenter> extends Fas
         return "";
     }
 
-    //重写该方法以自定Toolbar图标
-    protected int getNavIcon() {
-        return 0;
+    protected void initBarLayout() {
+
+        View barLayout;
+        List<View> list = getAllSomeView(rootView, AppBarLayout.class);
+        if (list == null || list.isEmpty())
+            barLayout = findViewById(getContext().getResources().getIdentifier("barLayout", "id", getContext().getPackageName()));
+        else
+            barLayout = list.get(0);
+
+        if (barLayout == null) return;
+
+        if (getBarLayoutBackgroundResource() != 0)
+            barLayout.setBackgroundResource(getBarLayoutBackgroundResource());
+        else
+            barLayout.setBackgroundColor(getBarLayoutBackgroundColor());
     }
 
     //重写该方法以自定Toolbar背景drawable
-    protected int getToolbarBackgroundResource(){
+    protected int getBarLayoutBackgroundResource(){
         return 0;
     }
 
     //重写该方法以自定Toolbar背景色
-    protected int getToolbarBackgroundColor(){
+    protected int getBarLayoutBackgroundColor(){
         return getContext().getResources().getColor(R.color.colorWidely);
+    }
+
+    //重写该方法以自定Toolbar图标
+    protected int getToolbarNavIcon() {
+        return 0;
     }
 
     //重写该方法以自定Toolbar控件颜色
@@ -113,14 +136,31 @@ public abstract class CustomBaseView<T extends ICustomBasePresenter> extends Fas
         return true;
     }
 
+    //指定控件具体类型，获取Container容器下所有该类型的控件
+    protected List getAllSomeView(View container, Class someView) {
+        List list = new ArrayList<>();
+        if (container instanceof ViewGroup)
+        {
+            ViewGroup viewGroup = (ViewGroup) container;
+            for (int i = 0; i < viewGroup.getChildCount(); i++)
+            {
+                View view = viewGroup.getChildAt(i);
+                if (someView.isAssignableFrom(view.getClass()))
+                    list.add(view);
+                //再次 调用本身（递归）
+                list.addAll(getAllSomeView(view,someView));
+            }
+        }
+        return list;
+    }
+
     private void setStatusBarLightMode(boolean isLightStyle){
         BarUtils.setStatusBarLightMode((Activity) getContext(),isLightStyle);
     }
 
     private void hideSystemBar() {
         BarUtils.setStatusBarAlpha((Activity) getContext(),0);
-        if (toolbar != null)
-            BarUtils.addPaddingTopEqualStatusBarHeight(toolbar);
+        if (toolbar != null) BarUtils.addPaddingTopEqualStatusBarHeight(toolbar);
     }
 
 }
